43d76635fceac670d2e814ac133c7e1a3ad6a304
Yousif AlOzairi <yoyo012@icloud.com>
2020-06-15 20:00:17 +0300

    added a randomizer for all inputs

--- calculator.js
+++ calculator.js
@@ -1,12 +1,13 @@
 // Define the numbers and the operation here
-let num1 = 5
-let num2 = 4
-let operation = "/"
+let num1 = Math.floor(Math.random() * 10)
+let num2 = Math.floor(Math.random() * 10)
+const operations = ["+", "-", "/", "*"]
+let randomOperation = operations[Math.floor(Math.random() * operations.length)]
 
 // Write if-statements here
 
-if (operation === "+") console.log(num1 + num2)
-else if (operation === "-") console.log(num1 - num2)
-else if (operation === "/") console.log(num1 / num2)
-else if (operation === "*") console.log(num1 * num2)
+if (randomOperation === "+") console.log(`${num1} added by ${num2} equals ${num1 + num2}`)
+else if (randomOperation === "-") console.log(`${num1} subtracted by ${num2} equals ${num1 - num2}`)
+else if (randomOperation === "/") console.log(`${num1} divided by ${num2} equals ${num1 / num2}`)
+else if (randomOperation === "*") console.log(`${num1} multiplied by ${num2} equals ${num1 * num2}`)
 else console.log("you can only add, multiply, subtract, or divide")
