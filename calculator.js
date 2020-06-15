910ca6c13cdd483f7b2c47bc39225f60ffda0488
Yousif AlOzairi <yoyo012@icloud.com>
2020-06-15 19:44:46 +0300

    created a calculator

--- calculator.js
+++ calculator.js
@@ -1,3 +1,12 @@
 // Define the numbers and the operation here
+let num1 = 5
+let num2 = 4
+let operation = "/"
 
 // Write if-statements here
+
+if (operation === "+") console.log(num1 + num2)
+else if (operation === "-") console.log(num1 - num2)
+else if (operation === "/") console.log(num1 / num2)
+else if (operation === "*") console.log(num1 * num2)
+else console.log("you can only add, multiply, subtract, or divide")
